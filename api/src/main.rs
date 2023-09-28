74bb05974d989ef8409b16a17c8021759b23f2b0
Alex Humphreys <alex.humphreys@pleo.io>
2023-09-28 21:37:50 +0200

    Add docker compose file

--- api/src/main.rs
+++ api/src/main.rs
@@ -13,7 +13,7 @@ async fn main() {
     let app = Router::new().route("/", get(handler));
 
     // run it
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
