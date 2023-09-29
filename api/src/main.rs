d19edb5cb6a5cded457bc4e7b620bcee329817ea
Alex Humphreys <alex.humphreys@pleo.io>
2023-09-29 10:15:18 +0200

    Add dot env and refactor observability setup

--- api/src/main.rs
+++ api/src/main.rs
@@ -5,21 +5,53 @@
 //! ```
 
 use axum::{response::Html, routing::get, Router};
+use dotenvy;
 use std::net::SocketAddr;
 
-use tower_http::{self, trace, trace::TraceLayer};
+use tower_http::{
+    self,
+    classify::{ServerErrorsAsFailures, SharedClassifier},
+    trace,
+    trace::TraceLayer,
+};
 use tracing::Level;
+
+fn setup_logging() {
+    let logging_format = std::env::var("LOGGING_FMT").unwrap_or("json".to_owned());
+
+    match logging_format.as_str() {
+        "json" => {
+            tracing_subscriber::fmt().with_target(false).json().init();
+        }
+        "pretty" => {
+            tracing_subscriber::fmt().with_target(false).pretty().init();
+        }
+        _ => {
+            tracing_subscriber::fmt()
+                .with_target(false)
+                .compact()
+                .init();
+        }
+    }
+}
+
+fn create_trace_layer(
+) -> TraceLayer<SharedClassifier<ServerErrorsAsFailures>, trace::DefaultMakeSpan> {
+    TraceLayer::new_for_http()
+        .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+        .on_response(trace::DefaultOnResponse::new().level(Level::INFO))
+}
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt().with_target(false).json().init();
+    dotenvy::dotenv().ok();
+
+    setup_logging();
 
     // build our application with a route
-    let app = Router::new().route("/", get(handler)).layer(
-        TraceLayer::new_for_http()
-            .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
-            .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
-    );
+    let app = Router::new()
+        .route("/", get(handler))
+        .layer(create_trace_layer());
 
     // run it
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
