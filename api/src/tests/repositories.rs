d973a10a5f2a36fed01d54d52250542e4a0778b3
Alex Humphreys <alex.humphreys@pleo.io>
2023-11-07 23:40:00 +0100

    Make tests compile

--- api/src/tests/repositories.rs
+++ api/src/tests/repositories.rs
@@ -1,24 +1,34 @@
+use crate::client_repository::MockClientRepo as MockClientRepoImpl;
 use crate::repositories::Repositories;
 use crate::user_repository::MockUserRepo as MockUserRepoImpl;
 
 pub async fn create_repositories_for_test() -> MockRepoImpls {
-    MockRepoImpls::new(MockUserRepoImpl::new())
+    MockRepoImpls::new(MockUserRepoImpl::new(), MockClientRepoImpl::new())
 }
 
 #[derive(Debug)]
 pub struct MockRepoImpls {
     pub user: MockUserRepoImpl,
+    pub client: MockClientRepoImpl,
 }
 impl MockRepoImpls {
-    pub fn new(mock_user_repo_impl: MockUserRepoImpl) -> Self {
+    pub fn new(
+        mock_user_repo_impl: MockUserRepoImpl,
+        mock_client_repo_impl: MockClientRepoImpl,
+    ) -> Self {
         Self {
             user: mock_user_repo_impl,
+            client: mock_client_repo_impl,
         }
     }
 }
 impl Repositories for MockRepoImpls {
     type UserRepoImpl = MockUserRepoImpl;
+    type ClientRepoImpl = MockClientRepoImpl;
     fn user(&self) -> &Self::UserRepoImpl {
         &self.user
+    }
+    fn client(&self) -> &Self::ClientRepoImpl {
+        &self.client
     }
 }
