d4eeedea6597d02ceaf714d279238a15bbcabf5e
Alex Humphreys <alex.humphreys@pleo.io>
2024-01-05 23:19:17 +0100

    remove .env from website

--- website/src/auth0.rs
+++ website/src/auth0.rs
@@ -13,7 +13,7 @@
 use crate::{app_state, handlers};
 use axum::{debug_handler, response::Redirect, routing::get, Router};
 use shared::error;
-use tower_http::services::ServeDir;
+use urlencoding::encode;
 
 /// Helper to create a random string 30 chars long.
 pub fn random_state_string() -> String {
@@ -21,6 +21,27 @@ pub fn random_state_string() -> String {
     let string = Alphanumeric.sample_string(&mut rand::thread_rng(), 30);
     println!("Random string: {}", string);
     string
+}
+
+/// Configuration state for Auth0, including the client secret, which
+/// must be kept private.
+struct AuthSettings {
+    client_id: String,
+    client_secret: String,
+    redirect_uri: String,
+    auth0_domain: String,
+}
+impl AuthSettings {
+    /// Given a state param, build a url String that our /auth0 redirect handler can use.
+    pub fn authorize_endpoint_url(&self, state: &str) -> String {
+        format!(
+            "https://{}/authorize?response_type=code&client_id={}&redirect_uri={}&scope=openid%20profile&state={}",
+            self.auth0_domain,
+            self.client_id,
+            encode(&self.redirect_uri),
+            state,
+        )
+    }
 }
 
 pub async fn router() -> Router {
@@ -35,6 +56,7 @@ pub async fn router() -> Router {
 #[debug_handler]
 pub async fn auth0_redirect() -> Result<Redirect, error::Error> {
     let state = random_state_string();
+    // cookies.add(Cookie::new("state", state.clone()));
 
     todo!()
 }
