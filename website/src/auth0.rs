1954bcfa7a0e10353f1ba5dd9fee84e127ec22c4
Alex Humphreys <alex.humphreys@pleo.io>
2024-01-05 18:36:22 +0100

    start adding auth0 redirect

--- website/src/auth0.rs
+++ website/src/auth0.rs
@@ -9,3 +9,32 @@
 //        static_files
 //    ]
 //}
+
+use crate::{app_state, handlers};
+use axum::{debug_handler, response::Redirect, routing::get, Router};
+use shared::error;
+use tower_http::services::ServeDir;
+
+/// Helper to create a random string 30 chars long.
+pub fn random_state_string() -> String {
+    use rand::distributions::{Alphanumeric, DistString};
+    let string = Alphanumeric.sample_string(&mut rand::thread_rng(), 30);
+    println!("Random string: {}", string);
+    string
+}
+
+pub async fn router() -> Router {
+    let auth0 = Router::new()
+        .route("/login", get(handlers::styles))
+        .route("/loggedin", get(handlers::greet))
+        .route("/auth0", get(auth0_redirect))
+        .route("/callback", get(handlers::styles));
+    auth0
+}
+
+#[debug_handler]
+pub async fn auth0_redirect() -> Result<Redirect, error::Error> {
+    let state = random_state_string();
+
+    todo!()
+}
