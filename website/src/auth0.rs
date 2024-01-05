c4514cbff7fdbdc19847b68efb67b10c9fc068ad
Alex Humphreys <alex.humphreys@pleo.io>
2024-01-06 00:29:50 +0100

    Start handling auth0 response, needs work

--- website/src/auth0.rs
+++ website/src/auth0.rs
@@ -11,7 +11,15 @@
 //}
 
 use crate::{app_state, handlers};
-use axum::{debug_handler, extract::State, response::Redirect, routing::get, Router};
+use axum::{
+    debug_handler,
+    extract::{Query, State},
+    response::Redirect,
+    routing::get,
+    Router,
+};
+use serde::{Deserialize, Serialize};
+use serde_json::ser::to_vec;
 use shared::error;
 use std::sync::Arc;
 use urlencoding::encode;
@@ -60,14 +68,28 @@ impl AuthSettings {
             state,
         )
     }
+    pub fn token_endpoint_url(&self) -> String {
+        format!("https://{}/oauth/token", self.auth0_domain)
+    }
+    /// Builds a TokenRequest from an authorization code and
+    /// Auth0 config values.
+    pub fn token_request(&self, code: &str) -> TokenRequest {
+        TokenRequest {
+            grant_type: String::from("authorization_code"),
+            client_id: self.client_id.clone(),
+            client_secret: self.client_secret.clone(),
+            code: code.to_string(),
+            redirect_uri: self.redirect_uri.clone(),
+        }
+    }
 }
 
 pub async fn router() -> Router {
     let auth0 = Router::new()
         .route("/auth0_login", get(handlers::styles))
-        .route("/loggedin", get(handlers::greet))
+        .route("/loggedin", get(handlers::styles))
         .route("/auth0", get(auth0_redirect))
-        .route("/callback", get(handlers::styles))
+        .route("/callback", get(callback))
         .with_state(app_state::create_app_state().await);
     auth0
 }
@@ -81,4 +103,49 @@ pub async fn auth0_redirect(
     let uri = data.auth0.authorize_endpoint_url(&state);
     println!("{:?}", uri);
     Ok(Redirect::to(&uri))
+}
+
+#[derive(Deserialize)]
+pub struct CallbackSchema {
+    code: String,
+    state: String,
+}
+
+/// Send TokenRequest to the Auth0 /oauth/token endpoint.
+#[derive(Serialize, Deserialize)]
+struct TokenRequest {
+    grant_type: String,
+    client_id: String,
+    client_secret: String,
+    code: String,
+    redirect_uri: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TokenResponse {
+    access_token: String,
+    expires_in: u32,
+    id_token: String,
+    token_type: String,
+}
+
+#[debug_handler]
+pub async fn callback(
+    auth0: Query<CallbackSchema>,
+    State(data): State<Arc<app_state::AppState>>,
+) -> Result<Redirect, error::Error> {
+    // TODO check state from cookie
+    let tr = data.auth0.token_request(&auth0.code);
+    let client = reqwest::Client::new();
+    let resp: TokenResponse = client
+        .post(data.auth0.token_endpoint_url())
+        .header("Content-Type", "application/json")
+        .body(to_vec(&tr).unwrap())
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    println!("{:?}", resp);
+    Ok(Redirect::to("/loggedin"))
 }
