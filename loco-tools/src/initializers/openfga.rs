7a7ef094188385a5eae2c3804a063354bf0ca1c3
Alex Humphreys <alex.humphreys@pleo.io>
2024-03-04 14:23:32 +0100

    Test initializer

--- loco-tools/src/initializers/openfga.rs
+++ loco-tools/src/initializers/openfga.rs
@@ -21,22 +21,31 @@ async fn create_or_get_store() -> Result<openfga::CreateDataStoreResponse> {
 
 async fn read_model_from_file() -> Result<String> {
     let model_string = std::fs::read_to_string("model.json")?;
-    todo!()
+    Ok(model_string)
 }
 
 async fn write_model(store_id: String, model: String) -> Result<WriteAuthorizationModelResponse> {
     let model = openfga::write_authorization_model(store_id, model, None).await;
-    todo!()
+    match model {
+        Ok(model) => {
+            println!("Model written successfully");
+            Ok(model)
+        }
+        Err(e) => {
+            println!("Error writing model: {:?}", e);
+            Err(loco_rs::Error::NotFound)
+        }
+    }
 }
 
-async fn initialize_model() -> Result<()> {
+async fn initialize_model() -> Result<(WriteAuthorizationModelResponse)> {
     // create or get store
     let store = create_or_get_store().await?;
     // read model from a file
     let model_string = read_model_from_file().await?;
 
-    let model = write_model(store.id.clone(), model_string).await;
-    todo!()
+    let model = write_model(store.id.clone(), model_string).await?;
+    Ok(model)
 }
 
 #[async_trait]
@@ -63,8 +72,9 @@ mod tests {
 
     #[tokio::test]
     async fn test_initializer() {
-        let res = initialize_model();
+        let res = initialize_model().await;
 
-        todo!()
+        println!("{:?}", res);
+        assert_eq!(false, true);
     }
 }
